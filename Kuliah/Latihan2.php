3d70412be12198280437d1306104d2c0f15a79bc
Richkyo <krich3156@gmail.com>
2025-02-17 13:02:47 +0700

    p

---
+++ Kuliah/Latihan2.php
@@ -0,0 +1,14 @@
+<?php
+
+$nama = "Rizky Putra prayoga";
+$email = "Krich3156@gmail.com";
+$pekerjaan = "Mahasiswa";
+$jurusan = "informatika";
+$universitas = "Universitas Padundan";
+
+
+echo "Halo, Nama saya $nama, saya 
+Saya adalah $pekerjaan di $universitas,
+Prodi saya adalah $jurusan"
+
+?>
---
+++ Kuliah/Pertemuan 2/Latihan1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php</title>
+</head>
+<body>
+<h1>
+    <?php 
+    echo "Helloworld";
+     ?>
+</h1>
+
+<h3>
+    Nama saya : <?php echo "Rizky"; ?>
+</h3>
+</body>
+</html>
