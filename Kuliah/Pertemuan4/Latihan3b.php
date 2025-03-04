7c91be31119b5182eecc56758cd29b4b0384ed98
Richkyo <krich3156@gmail.com>
2025-03-04 23:52:21 +0700

    Menyelesaikan tugas 3

---
+++ Kuliah/Pertemuan4/Latihan3b.php
@@ -0,0 +1,4 @@
+<?php
+for($i = 1; $i < 12; $i++ ) {
+    echo "hello world $i x<br> <hr>";
+}
---
+++ Kuliah/Pertemuan4/Latihan3d.php
@@ -0,0 +1,12 @@
+<?php
+// pengulangan // loop // looping
+// while //
+// komponen while
+// 1. nilai awal / inisialisasi
+// 2. kondsi terminasi / kapan harus berenti
+// 3. increment / decrement
+$nilai_awal = 1;
+while ($nilai_awal >= 1) {
+    echo "hello world $nilai_awal X <br>";
+    $nilai_awal = $nilai_awal - 1;
+}
