0df4e101029d6459eb5b984f7e42159c6a945c35
Richkyo <krich3156@gmail.com>
2025-03-14 20:30:59 +0700

    Menyelesaikan Tugas 4

---
+++ Tugas/Tugas/Tugas4/Tugas4_a.php
@@ -0,0 +1,6 @@
+<?php
+// Membuat array dengan 6 elemen string
+$kata = ["epan", "cipta", "akan", "u", "asa", "esok"];
+
+// Menampilkan kalimat dengan menggunakan elemen array
+echo "Masa depanmu d" . $kata[0] . " d" . $kata[1] . " oleh apa yang kau " . $kata[2] . " hari ini, bukan " . $kata[5] . ".";
